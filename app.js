3cc60e44e433ef5e61dbcf0978983460968b0b49
Shailen85 <112659669+Shailen85@users.noreply.github.com>
2024-02-24 08:36:53 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -113,9 +113,9 @@ const addCartToHTML = () => {
                         ${info.price * item.quantity}
                         </div>
                         <div class="quantity">
-                            <span class=${minus}"><</span>
+                            <span class="${minus}"><</span>
                             <span>${item.quantity}</span>
-                            <span class="${plus}">></span>
+                            <span class="plus">></span>
                         </div>
                     </div>
                 </div>
@@ -133,9 +133,9 @@ const addCartToHTML = () => {
                 // </div>
                 // <div class="totalPrice">$${info.price * item.quantity}</div>
                 // <div class="quantity">
-                //     <span class="${type.minus}"><</span>
+                //     <span class="minus"><</span>
                 //     <span>${item.quantity}</span>
-                //     <span class="${type.plus}">></span>
+                //     <span class="plus">></span>
                 // </div>
             `;
         });
